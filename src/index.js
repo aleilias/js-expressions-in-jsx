52445c1d308e288bafd70ac31f21f7cf04a9c497
aleilias <86530708+aleilias@users.noreply.github.com>
2021-12-23 15:53:07 -0600

    js expressions

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ const luckyNumber = Math.floor(Math.random() * 10);
 
 ReactDOM.render(
   <div>
-    <h1>
-      Hello {fName} {lName}!
-    </h1>
+    <h1>Hello {`${fName} ${lName}`}!</h1>
     <p>Your lucky number is {luckyNumber}</p>
   </div>,
   document.getElementById("root")
